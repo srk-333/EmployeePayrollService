2e34201d70b239318081d9e3823dbeba51b3c532
srk-333 <sauravmk333@gmail.com>
2022-02-18 10:21:17 +0530

    [Saurav]Add:Welcome Message

---
+++ EmployeePayrollService/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeePayrollService
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Payroll Service");
+            Console.ReadKey();
+        }
+    }
+}
